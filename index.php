0fe84628dc252f39776a5bb4ef4aa778b2f9c3b7
realwashcar <apipath.k@kkumail.com>
2023-11-21 06:01:38 +0700

    new

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$serverName = "119.59.96.62"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"drnadech_research", "UID"=>"TRANSF", "PWD"=>"Ed1p663b^");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+     $tsql = "SELECT * FROM DATA_DOMAIN"; 
+     $stmt = sqlsrv_query( $conn, $tsql); 
+     while( $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+          echo $row['code'].", ".$row['Vname'].""."<br />";
+      }
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
